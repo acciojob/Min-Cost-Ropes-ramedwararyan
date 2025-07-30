6d6cbf92d41216408dae251e21d4aae083dd3cab
ramedwararyan <143419537+ramedwararyan@users.noreply.github.com>
2025-07-31 02:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,87 @@
-function mincost(arr) {
-  if (arr.length <= 1) return 0;
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-  // Create a min-heap by sorting initially
-  arr.sort((a, b) => a - b);
+  // Helper methods
+  getParentIndex(i) { return Math.floor((i - 1) / 2); }
+  getLeftChildIndex(i) { return 2 * i + 1; }
+  getRightChildIndex(i) { return 2 * i + 2; }
+
+  // Swap helper
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  // Add element
+  push(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  // Remove smallest element
+  pop() {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return root;
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      let parentIndex = this.getParentIndex(index);
+      if (this.heap[parentIndex] <= this.heap[index]) break;
+      this.swap(parentIndex, index);
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let smallerChildIndex = this.getLeftChildIndex(index);
+      if (
+        this.getRightChildIndex(index) < this.heap.length &&
+        this.heap[this.getRightChildIndex(index)] < this.heap[smallerChildIndex]
+      ) {
+        smallerChildIndex = this.getRightChildIndex(index);
+      }
+      if (this.heap[index] <= this.heap[smallerChildIndex]) break;
+      this.swap(index, smallerChildIndex);
+      index = smallerChildIndex;
+    }
+  }
+}
+
+// Main mincost function
+function mincost(arr) {
+  const minHeap = new MinHeap();
+
+  // Step 1: Add all elements to heap
+  for (let num of arr) {
+    minHeap.push(num);
+  }
 
   let totalCost = 0;
 
-  while (arr.length > 1) {
-    // Take two smallest ropes
-    let first = arr.shift();
-    let second = arr.shift();
+  // Step 2: Combine ropes until one remains
+  while (minHeap.size() > 1) {
+    const first = minHeap.pop();
+    const second = minHeap.pop();
 
-    let cost = first + second;
+    const cost = first + second;
     totalCost += cost;
 
-    // Insert the new rope length into the sorted array
-    arr.push(cost);
-    arr.sort((a, b) => a - b); // maintain min-heap property using sort
+    // Add new rope length back to heap
+    minHeap.push(cost);
   }
 
   return totalCost;
